ea3bc0676fd67cc31e71ceae964096b0addaafe0
liorzenator <leorz@me.com>
2026-02-08 17:13:31 +0200

    update swagger server URL to use PORT environment variable directly

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 8080}`,
+        url: `http://localhost:${process.env.PORT}`,
       },
     ],
   },
